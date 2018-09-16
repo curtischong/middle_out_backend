5897b4f38cbdd6e71304a50a5d4b4e165a0eecf9
Curtis Chong <curtischong5@gmail.com>
2018-09-16 05:27:41 -0400

    cleaned up repo

--- auto_style.py
+++ auto_style.py
@@ -6,4 +6,4 @@ included_extensions = ['jpg', 'bmp', 'png', 'gif']
 onlyfiles = [fn for fn in os.listdir(path)
                 if any(fn.endswith(ext) for ext in included_extensions)]
 for one_file in onlyfiles:
-    subprocess.call(["python3", "neural_style.py", "--content", "in/out.png", "--styles", "examples/2-style1.jpg", "--output", "out/out.png", "--overwrite", "--iterations", "100"])
+    subprocess.call(["python3", "neural_style.py", "--content", "style_input/" + one_file, "--styles", "examples/2-style1.jpg", "--output", "style_output/" + one_file, "--overwrite", "--iterations", "100"])
