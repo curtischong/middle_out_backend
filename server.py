9caa2ba5e49c9a5ad5995c70fc4827d5e442045b
Marcel O'Neil <marcel@marceloneil.com>
2018-09-16 00:25:04 -0400

    database

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, g
+from flask_cors import CORS
 from PIL import Image
 from io import BytesIO
 import base64
@@ -6,24 +7,46 @@ import sqlite3
 import hashlib
 
 app = Flask(__name__)
+# CORS(app)
+
 conn = sqlite3.connect('frames.db')
 c = conn.cursor()
-c.execute('CREATE TABLE frames (index real, filename text)')
-c.commit()
+c.execute("SELECT 1 FROM sqlite_master WHERE type='table' AND name='frames'")
+if not c.fetchone():
+    c.execute('CREATE TABLE frames (frame real, filename text)')
+conn.commit()
+c.close()
+conn.close()
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect('frames.db')
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.commit()
+        db.close()
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    json = request.get_json()
-    index = int(json['index'])
-    raw = BytesIo(base64.b64decode(json['uri'].split(',')[1]))
-    img = Image.open(raw)
+    data = request.get_json()
+    index = int(data['index'])
+    raw = base64.b64decode(data['uri'].split(',')[1])
     checksum = hashlib.sha256()
     checksum.update(raw)
     name = checksum.hexdigest()
-    img.save('/var/www/static/{:s}.png'.format(name))
-    c.execute('INSERT INTO frames (?,?)', (index, name,))
-    c.commit()
-    return jsonify({'file': '{:s}.png'.format(name)}), 200
+    img = Image.open(BytesIO(raw))
+    bg = Image.new('1', (img.size[0], img.size[1]), 256)
+    bg.paste(img, mask=img)
+    bg.save('/var/www/static/{:s}.png'.format(name), 'PNG')
+    c = get_db().cursor()
+    c.execute('INSERT INTO frames VALUES (?,?)', (index, name,))
+    c.close()
+    return jsonify({'file': 'http://api.piepiper.1lab.me/static/{:s}.png'.format(name)}), 200
 
 if __name__ == '__main__':
     app.run()
